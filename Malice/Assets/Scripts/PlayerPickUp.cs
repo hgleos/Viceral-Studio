ee5da7da60ac893225a588d50bd379d0af69b448
aporria <aporria0451@gmail.com>
2022-11-28 16:35:03 -0800

    Added enemy AI as well as obstacle logic

--- Malice/Assets/Scripts/PlayerPickUp.cs
+++ Malice/Assets/Scripts/PlayerPickUp.cs
@@ -5,57 +5,30 @@ using UnityEngine;
 public class PlayerPickUp : MonoBehaviour
 {
     [SerializeField] private LayerMask pickableLayerMask;
-    [SerializeField] private LayerMask interactiveLayerMask;
     [SerializeField] private Transform playerCameraTransform;
     [SerializeField] GameObject pickUpUI;
-    [SerializeField] GameObject interactUI;
     [SerializeField] [Min(1)] private float pickUpRange = 3;
     [SerializeField] ShowHide showHide;
-    [SerializeField] KeyBehavior keyBehavior;
 
     private RaycastHit hit;
 
     private void Update()
     {
         if (hit.collider != null)
-        {
             pickUpUI.SetActive(false);
-            interactUI.SetActive(false);
-        }
-        
+
         if(Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, pickUpRange, pickableLayerMask))
         {
             pickUpUI.SetActive(true);
             if(Input.GetKeyDown(KeyCode.E))
             {
-                PickUp(hit.collider.tag);
+                PickUp();
             }
         }
-
-        else if(Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, pickUpRange, interactiveLayerMask))
-        {
-            interactUI.SetActive(true);
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                Interact();
-            }
-        }
-
     }
 
-    private void PickUp(string tag)
+    private void PickUp()
     {
-        if (tag == "Flashlight")
-        {
-            showHide.hasFlashLight = true;
-
-        }
-        else if (tag == "LevelOneKey")
-            keyBehavior.hasLevelOnekey = true;
-    }
-
-    private void Interact()
-    {
-        print("interact");
+        showHide.hasFlashLight = true;
     }
 }
