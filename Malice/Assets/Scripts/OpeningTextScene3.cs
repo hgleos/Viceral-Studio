c51ccf1c1209754027881b7622a64f1edbda18c9
aporria <aporria0451@gmail.com>
2022-12-10 18:10:53 -0800

    Added enemy AI and waypoints to level 2

---
+++ Malice/Assets/Scripts/OpeningTextScene3.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningTextScene3 : MonoBehaviour
+{
+    public GameObject TextBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1f);
+        TextBox.GetComponent<Text>().text = "What was that thing?";
+        yield return new WaitForSeconds(3f);
+        TextBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(1f);
+        TextBox.GetComponent<Text>().text = "I've got to be close to exit by now...";
+        yield return new WaitForSeconds(3f);
+        TextBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(1f);
+        TextBox.GetComponent<Text>().text = "The voices are getting louder...";
+        yield return new WaitForSeconds(3f);
+        TextBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(1f);
+        TextBox.GetComponent<Text>().text = "I NEED to get out of here.";
+        yield return new WaitForSeconds(3f);
+        TextBox.GetComponent<Text>().text = "";  
+    }
+}
